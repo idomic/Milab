3e02fddfb7925182329e5bb33ccd67686afe3672
Nir <nirsamet@gmail.com>
2015-03-27 12:22:04 +0300

    last for today
    creating the new app with new dropbox user

--- Android_sdk/dropbox-android-sdk-1.6-2.3/examples/DBRoulette/src/com/android/wephone/DBRoulette.java
+++ Android_sdk/dropbox-android-sdk-1.6-2.3/examples/DBRoulette/src/com/android/wephone/DBRoulette.java
@@ -69,8 +69,8 @@ public class DBRoulette extends Activity {
     // Note that this is a really insecure way to do this, and you shouldn't
     // ship code which contains your key & secret in such an obvious way.
     // Obfuscation is good.
-    private static final String APP_KEY =    "mz8459n9xekzmvz";
-    private static final String APP_SECRET = "11enl68c1x35nvk";
+    private static final String APP_KEY =    "c3ni1eiw3ackhz5";
+    private static final String APP_SECRET = "io385nt1sgfs2qb";
 
     ///////////////////////////////////////////////////////////////////////////
     //                      End app-specific settings.                       //
